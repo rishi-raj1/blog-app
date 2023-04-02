87758e093975822af95683d2b72fc9c6c23d2fa2
RISHI RAJ <rishiraj@gmail.com>
2023-04-02 23:16:35 +0530

    changed apiurl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = 'http://localhost:5000';
+// const API_URL = 'http://localhost:5000';
+const API_URL = 'https://backendblog-sask.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
